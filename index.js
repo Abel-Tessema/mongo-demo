8e13416484651dfb0df925e899c6f588aff667d8
Bela Jash <abelxotessema@gmail.com>
2024-08-13 14:14:31 +0300

    Schemas.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
   .then(() => console.log('MongoDB Connected...'))
   .catch(error => console.error('Something\'s not right.', error));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {type: Date, default: Date.now},
+  isPublished: Boolean
+});
