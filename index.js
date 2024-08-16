4ca136a04bc7f2629408d8754bb2416e23c78289
Bela Jash <abelxotessema@gmail.com>
2024-08-16 15:30:01 +0300

    Comparison Query Operators.

--- index.js
+++ index.js
@@ -27,8 +27,18 @@ async function createCourse() {
 }
 
 async function getCourses() {
+  // eq = equal
+  // ne = not equal
+  // gt = greater than
+  // gte = greater than or equal to
+  // lt = less than
+  // lte = less than or equal to
+  // in = in
+  // nin = not in
   const courses = await Course
-    .find({author: 'Bela Jash', isPublished: true})
+    // .find({author: 'Bela Jash', isPublished: true})
+    // .find({price: {$gte: 10, $lte: 20}})
+    .find({price: {$in: [10, 15, 20]}})
     .limit(2)
     .sort({name: 1})
     .select({name: 1, tags: 1});
